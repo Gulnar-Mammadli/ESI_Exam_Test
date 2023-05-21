378e7144da98a8db743ab7a59840afeed621f984
mammadli <gulnar.mammadli@ut.ee>
2023-05-21 18:39:49 +0300

    Add makePayment method

--- ms-payment/src/main/java/com/mammadli/mspayment/service/PaymentService.java
+++ ms-payment/src/main/java/com/mammadli/mspayment/service/PaymentService.java
@@ -1,7 +1,42 @@
 package com.mammadli.mspayment.service;
 
+import com.mammadli.mspayment.dto.OrderDto;
+import com.mammadli.mspayment.dto.OrderStatus;
+import com.mammadli.mspayment.dto.PaymentStatus;
+import com.mammadli.mspayment.model.UserBalance;
+import com.mammadli.mspayment.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class PaymentService {
+    private final PaymentRepository paymentRepository;
+
+    public void makePayment(OrderDto orderDto){
+        Optional<UserBalance> userBalance = paymentRepository.findById(orderDto.getOrderId());
+        if(userBalance.isPresent()){
+
+            if (userBalance.get().getBalance().compareTo(orderDto.getPrice()) == 1) {
+                userBalance.get().setBalance(userBalance.get().getBalance().subtract(orderDto.getPrice()));
+                paymentRepository.save(userBalance.get());
+
+                orderDto.setPaymentStatus(PaymentStatus.COMPLETED);
+                orderDto.setOrderStatus(OrderStatus.COMPLETED);
+
+//                kafkaTemplate.send("paymentTopic", orderDto);
+
+            } else {
+
+                orderDto.setPaymentStatus(PaymentStatus.FAILED);
+                orderDto.setOrderStatus(OrderStatus.CANCELLED);
+
+//                kafkaTemplate.send("paymentTopic", orderDto);
+            }
+
+        }
+    }
+
 }
