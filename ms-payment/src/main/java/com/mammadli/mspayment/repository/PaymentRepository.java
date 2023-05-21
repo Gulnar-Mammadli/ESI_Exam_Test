806204564c458d99560b4d851b59f010c566aa0e
mammadli <gulnar.mammadli@ut.ee>
2023-05-21 18:23:54 +0300

    PaymentRepository and PaymentService added

--- ms-payment/src/main/java/com/mammadli/mspayment/repository/PaymentRepository.java
+++ ms-payment/src/main/java/com/mammadli/mspayment/repository/PaymentRepository.java
@@ -1,7 +1,9 @@
 package com.mammadli.mspayment.repository;
 
+import com.mammadli.mspayment.model.UserBalance;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentRepository {
+public interface PaymentRepository extends JpaRepository<UserBalance,Long> {
 }
